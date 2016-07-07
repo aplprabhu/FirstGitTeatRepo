4cfc0b258d2e8dfd0274f7c3b19162691f6b21e9
Prabhu <prabhu.ponnusamy@capgemini.com>
2016-07-07 10:04:27 +0530

    Initial Commit

---
+++ ProductApp/src/org/cap/demo/MainClass.java
@@ -0,0 +1,15 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		ProductApp app = new ProductApp();
+		app.productDisplay();
+		greetings();
+	}
+
+	public static void greetings() {
+		System.out.println("Happy to create my fiorst Git app");
+	}
+
+}
---
+++ ProductApp/src/org/cap/demo/ProductApp.java
@@ -0,0 +1,11 @@
+package org.cap.demo;
+
+public class ProductApp {
+
+	public void productDisplay() {
+		System.out.println("IGATE");
+		System.out.println("Capgemini");
+
+	}
+
+}
