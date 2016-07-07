4ed4df03e6ff0f842f9160be440d4e14d74ecbc2
Prabhu <prabhu.ponnusamy@capgemini.com>
2016-07-07 11:03:12 +0530

    updated variable

--- ProductApp/src/org/cap/demo/ProductApp.java
+++ ProductApp/src/org/cap/demo/ProductApp.java
@@ -8,8 +8,8 @@ public class ProductApp {
 		System.out.println("IGATE");
 		System.out.println("Capgemini");
 		
-		for(int j1=0;j1<arr.length;j1++){
-			System.out.println("Date"+arr[j1]);
+		for(int j2=0;j2<arr.length;j2++){
+			System.out.println("Date"+arr[j2]);
 		}
 	}
 		
