7e042c3d3cdb6c960b339eaa4554bd096a34f88f
Prabhu <prabhu.ponnusamy@capgemini.com>
2016-07-07 10:23:55 +0530

    Update productDisplay

--- ProductApp/src/org/cap/demo/ProductApp.java
+++ ProductApp/src/org/cap/demo/ProductApp.java
@@ -2,10 +2,15 @@ package org.cap.demo;
 
 public class ProductApp {
 
+	int[] arr = {1,4,23};
+	
 	public void productDisplay() {
 		System.out.println("IGATE");
 		System.out.println("Capgemini");
-
+		
+		for(int j=0;j<arr.length;j++){
+			System.out.println("Date"+arr[j]);
+		}
 	}
-
+		
 }
